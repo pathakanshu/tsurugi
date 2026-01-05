468a55047ecf53961af0d2719280738cf997756b
Anshu Pathak <anshuapr@gmail.com>
2026-01-05 13:52:26 -0500

    fix: fixed path abs issue

--- src/tsurugi/mcserver.py
+++ src/tsurugi/mcserver.py
@@ -28,8 +28,10 @@ def _get_start_cmd():
     """Build the start command from config."""
     config = _load_config()
 
-    jar_full_path = os.path.expanduser(
-        config["minecraft"]["path"] + config["minecraft"]["jar_file_name"]
+    jar_full_path = os.path.abspath(
+        os.path.expanduser(
+            config["minecraft"]["path"] + config["minecraft"]["jar_file_name"]
+        )
     )
     return [
         "screen",
