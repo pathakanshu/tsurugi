691299e9dfb9d315a02456a2aec0419f388c1e63
Anshu Pathak <anshuapr@gmail.com>
2026-01-05 10:03:38 -0500

    fix: fixed wrong env var name

--- src/tsurugi/config.py
+++ src/tsurugi/config.py
@@ -5,6 +5,6 @@ import dotenv
 # Load environment variables from a .env file
 dotenv.load_dotenv()
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+DISCORD_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 if not DISCORD_TOKEN:
-    raise RuntimeError("DISCORD_TOKEN not set")
+    raise RuntimeError("DISCORD_BOT_TOKEN not set")
